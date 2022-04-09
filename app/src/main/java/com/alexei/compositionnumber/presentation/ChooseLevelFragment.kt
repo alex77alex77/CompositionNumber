d2eb2a7bc58ab4231357610c096a1d8dfb364e57
login <gribal3x@yandex.ru>
2022-04-09 14:25:59 +0400

    created project

--- app/src/main/java/com/alexei/compositionnumber/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/alexei/compositionnumber/presentation/ChooseLevelFragment.kt
@@ -1,14 +1,13 @@
 package com.alexei.compositionnumber.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.alexei.compositionnumber.R
+import androidx.fragment.app.Fragment
 import com.alexei.compositionnumber.databinding.FragmentChooseLevelBinding
 
-class ChooseLeveFragment : Fragment() {
+class ChooseLevelFragment : Fragment() {
     private var _binding: FragmentChooseLevelBinding? = null
     private val binding: FragmentChooseLevelBinding
         get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
@@ -16,11 +15,6 @@ class ChooseLeveFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
     }
 
@@ -38,4 +32,9 @@ class ChooseLeveFragment : Fragment() {
         _binding = null
     }
 
+    companion object {
+        fun newInstance(): ChooseLevelFragment {
+            return ChooseLevelFragment()
+        }
+    }
 }
